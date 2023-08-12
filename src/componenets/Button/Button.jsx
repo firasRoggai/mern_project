caa841ee7cdaab2cf58f61f70e9c9bc0d0d23bc6
frantic-dev <107622593+frantic-dev@users.noreply.github.com>
2023-08-12 19:38:16 +0100

    minor button component fixes

--- src/componenets/Button/Button.jsx
+++ src/componenets/Button/Button.jsx
@@ -1,6 +1,7 @@
 import './Button.scss'
-export default function Button(props) {
-  const classes = `button ${props.buttonBgColor}`
+
+export default function Button({buttonBgColor}) {
+  const classes = `button ${buttonBgColor}`
   return (
       <a href="#" className={classes}>see product</a>
   )
