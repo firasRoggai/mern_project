a3bfdf3d1fb2180b76e9d4128abbac07db4bf850
frantic-dev <107622593+frantic-dev@users.noreply.github.com>
2023-08-13 17:54:45 +0100

    making category cards components responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import Navbar from './componenets/Navbar/Navbar'
-import Button from './componenets/Button/Button'
 import './style/main.scss'
 import CategoryCards from './componenets/CategoryCards/CategoryCards'
-import headphonesImg from './assets/headphones-img.png'
 
 function App() {
-	return (
-		<>
-    <Navbar />
-		<CategoryCards img={headphonesImg} title="headphones" />
-		</>
-	)
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <CategoryCards />
+      </div>
+    </>
+  )
 }
 
 export default App
